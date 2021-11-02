c1e408d5bc8409eac99f9d0fc31fe056fc6b46c4
Blyaodun <kozlovis88@mail.ru>
2021-11-02 18:51:37 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use sctrict";
+
+//const answer = +prompt("Вам есть 18?", "");
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt("Как Ваше имя?", "");
+//answers[1] = prompt("Как Ваша фамилия?", "");
+//answers[2] = prompt("Сколько Вам лет?", "");
+
+console.log(typeof(answers));
